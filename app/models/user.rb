a48208add40599afc4e6acdb079f681be35111a8
Yuki Kayama <kayama0204@gmail.com>
2018-05-28 07:34:27 +0900

    TweetとUserとの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
   has_one_attached :avatar
+  has_many :tweets, dependent: :destroy
 end
