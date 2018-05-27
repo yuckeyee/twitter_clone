cf027d31a55ee0d34328bf605a215f3295a3c711
Yuki Kayama <kayama0204@gmail.com>
2018-05-27 14:00:26 +0900

    Userモデルのエラー処理

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,16 @@
+module DeviseHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+
+    html = []
+    html << content_tag(:div, class: 'alert alert-danger', role: 'alert') do
+      content_tag(:ul) do
+        resource.errors.full_messages.each do |error_message|
+          concat content_tag(:li, error_message.to_s)
+        end
+      end
+    end
+
+    safe_join html
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
 end
