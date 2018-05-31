f2c022f94764f86f576057e5cd74d5d0cbfa9a2a
Yuki Kayama <kayama0204@gmail.com>
2018-05-31 17:24:32 +0900

    tweet投稿機能の作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,20 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.order('created_at desc')
   end
+
+  def create
+    if current_user.tweets.create(tweet_params)
+      flash[:notice] = '投稿しました'
+    else
+      flash[:error] = '投稿に失敗しました'
+    end
+
+    redirect_to :root
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
 end
