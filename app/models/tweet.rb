770156dda68823924641f193b02fc44016f16088
Yuki Kayama <kayama0204@gmail.com>
2018-06-06 07:46:38 +0900

    tweetの投稿を280文字以内に制限

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 280 }
   scope :recent, -> { order('created_at desc') }
 end
