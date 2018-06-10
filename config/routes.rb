1ac2aea348aa605f8f71295bb31e542833a62754
Yuki Kayama <kayama0204@gmail.com>
2018-06-10 09:56:00 +0900

    user一覧表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :users, only: %i(show)
+  resources :users, only: %i(index show)
   resources :tweets, only: %i(create destroy)
 end
