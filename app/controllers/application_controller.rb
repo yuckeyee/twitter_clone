b7208e7d9b7753534990753581923ca2f7a94922
Yuki Kayama <kayama0204@gmail.com>
2018-06-11 20:22:47 +0900

    userテーブルにprofileを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = %i[name email password password_confirmation remember_me avatar]
+    added_attrs = %i[name email password password_confirmation remember_me avatar profile]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
