2a1f4da23cfb50a154e5bb7489ceff4f1cd4cb11
Yuki Kayama <kayama0204@gmail.com>
2018-06-13 08:05:20 +0900

    userのfeatureスペックにuserの検索テスト追加

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -10,4 +10,18 @@ RSpec.feature "Users", type: :feature do
 
     expect(page).to have_selector('.list-group-item', count: 8)
   end
+
+  scenario "search users" do
+    user = create(:user)
+    login_as user, scope: :user
+
+    create(:user, name: 'Bob')
+    create(:user, profile: 'Bob')
+    create(:user, name: 'John', profile: 'John')
+    visit users_path
+
+    fill_in "search", with: 'Bob'
+    click_button "キーワード検索"
+    expect(page).to have_selector('.list-group-item', count: 4)
+  end
 end
