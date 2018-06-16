8738df7af5f62fe3e2aafbe2b86faa865a2222ff
Yuki Kayama <kayama0204@gmail.com>
2018-06-16 21:06:50 +0900

    UserとRelationshipの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   has_one_attached :avatar
   has_many :tweets, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id',
+                                  dependent: :destroy, inverse_of: :active_relationships
 
   scope :recent, -> { order('created_at desc') }
   scope :search, ->(keyword) {
