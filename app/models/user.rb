1b0959f5cfdf82566c64b75ea99fc2eb91f7695f
Yuki Kayama <kayama0204@gmail.com>
2018-06-17 19:35:29 +0900

    user_infoのパーシャル追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_many :tweets, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
-  has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
@@ -18,14 +18,14 @@ class User < ApplicationRecord
   }
 
   def follow(other_user)
-    self.active_relationships.create(followed_id: other_user.id)
+    active_relationships.create(followed_id: other_user.id)
   end
 
   def unfollow(other_user)
-    self.active_relationships.find_by(followed_id: other_user.id).destroy
+    active_relationships.find_by(followed_id: other_user.id).destroy
   end
 
   def following?(other_user)
-    self.following.include?(other_user)
+    following.include?(other_user)
   end
 end
