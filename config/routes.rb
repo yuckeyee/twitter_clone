91e79442aa93f58765d9f9d16e195ef05f97a3d0
Yuki Kayama <kayama0204@gmail.com>
2018-06-17 20:26:03 +0900

    フォローとフォロワーページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :users, only: %i(index show)
+  resources :users, only: %i(index show) do
+    member do
+      get :following, :followers
+    end
+  end
   resources :tweets, only: %i(create destroy)
 end
