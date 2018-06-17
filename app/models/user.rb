70f0fc40445e0ec58918bb573d653d1cb61d4cc9
Yuki Kayama <kayama0204@gmail.com>
2018-06-17 17:12:38 +0900

    フォローされているユーザの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_many :tweets, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   scope :recent, -> { order('created_at desc') }
   scope :search, ->(keyword) {
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,6 +66,7 @@ RSpec.describe User, type: :model do
       it 'returns true that use following? method' do
         user1.follow(user2)
         expect(user1.following?(user2)).to be true
+        expect(user2.followers.include?(user1)).to be true
       end
     end
 
@@ -74,6 +75,7 @@ RSpec.describe User, type: :model do
         user1.follow(user2)
         user1.unfollow(user2)
         expect(user1.following?(user2)).to be false
+        expect(user2.followers.include?(user1)).to be false
       end
     end
   end
